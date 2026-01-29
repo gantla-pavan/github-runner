e10356a75da04555b153a50aeffd2d35039f7fc0
gantlapavan <gantlapavan1705@gmail.com>
2026-01-29 15:44:52 +0530

    some message

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "daws86s.cyou"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z07796551BVY32FNM4MO7"
+  description = "description"
+}
